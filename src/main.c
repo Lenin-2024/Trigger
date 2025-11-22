f7459e7d36e409ef1ac90002586cf837c0ce6d96
Lenin-2024 <amelkin.misha@mail.ru>
2025-11-22 18:54:55 +0300

    Keystroke handling has been updated and reworked and moved into a subroutine

--- src/main.c
+++ src/main.c
@@ -14,8 +14,124 @@ int cmd_pos = 0;
 
 char last_command[256] = "";
 
-char output_buf[8192] = "";
+char output_buf[BUFSIZ] = "";
 int output_len = 0;
+
+void updateInput(int pipe_to);
+char* clear_str(char *output_start);
+void drawGame();
+
+int main() {
+    InitWindow(800, 600, "IPC");
+
+    int stdin_pipe[2], stdout_pipe[2];    
+    if (pipe(stdin_pipe) == -1 || pipe(stdout_pipe) == -1) {
+        perror("pipe");
+        exit(1);
+    }
+
+    fcntl(stdout_pipe[0], F_SETFL, O_NONBLOCK);
+    fcntl(STDIN_FILENO, F_SETFL, O_NONBLOCK);
+
+    pid_t pid = fork();
+    if (pid == 0) {
+        close(stdin_pipe[1]);
+        close(stdout_pipe[0]);
+
+        dup2(stdin_pipe[0], STDIN_FILENO);
+        dup2(stdout_pipe[1], STDOUT_FILENO);
+        dup2(stdout_pipe[1], STDERR_FILENO);
+
+        close(stdin_pipe[0]);
+        close(stdout_pipe[1]);
+
+        execl("./tinyemu-2019-12-21/temu", "./temu", "-ctrlc", "./conf/root-riscv64.cfg", NULL);
+        perror("execl");
+        exit(1);
+    } else {
+        close(stdin_pipe[0]);
+        close(stdout_pipe[1]);
+
+        printf("Temu запущен (PID: %d)\n", pid);
+        printf("Готов к приему команд:\n");
+
+        while (!WindowShouldClose()) {
+            /*----------обработка ввода от пользователя----------*/
+            updateInput(stdin_pipe[1]);
+            /*----------конец обработки ввода----------*/
+
+            /*----------начало чтения----------*/
+            char buffer[1024];
+            ssize_t bytes = read(stdout_pipe[0], buffer, sizeof(buffer)-1);
+            if (bytes > 0) {
+                buffer[bytes] = '\0';
+                char *cleaned = clear_str(buffer);
+
+                size_t clean_len = strlen(cleaned);
+                if (output_len + clean_len < sizeof(output_buf) - 1) {
+                    strcat(output_buf, cleaned);
+                    output_len += clean_len;
+                } else {
+                    memset(output_buf, 0, BUFSIZ);
+                    output_len = 0;
+                }
+
+                // printf("cleaned: '%s'\n", cleaned);
+                // fflush(stdout);
+            } else if (bytes < 0 && errno != EAGAIN) {
+                perror("read stdout");
+                break;
+            } else if (bytes == 0) {
+                printf("Temu завершил работу\n");
+                break;
+            }
+            /*----------завершение чтения----------*/
+
+            drawGame();
+        }
+
+        close(stdin_pipe[1]);
+        close(stdout_pipe[0]);
+
+        int status;
+        if (waitpid(pid, &status, 0)) {
+            printf("Child exit status - %d\n", WEXITSTATUS(status));
+        }
+        printf("Temu завершен.\n");
+    }
+
+    CloseWindow();
+    return 0;
+}
+
+void updateInput(int pipe_to) {
+    int key = GetCharPressed();
+    if ((key > 0) && (cmd_pos < sizeof(command) - 1)) {
+        command[cmd_pos++] = key;
+    }
+
+    if (IsKeyPressed(KEY_ENTER)) {
+        command[cmd_pos] = '\0';
+                    
+        if (strcmp(command, "exit") == 0) {
+            //break;
+        }
+                    
+        strcpy(last_command, command);
+
+        output_buf[0] = '\0';
+        write(pipe_to, command, cmd_pos);
+        write(pipe_to, "\n", 1);
+
+        memset(command, 0, 256);
+
+        cmd_pos = 0;
+    }
+
+    if (IsKeyPressed(KEY_BACKSPACE) && cmd_pos > 0) {
+        command[--cmd_pos] = '\0';
+    }
+}
 
 char* clear_str(char *output_start) {
     char *result = output_start;
@@ -28,6 +144,10 @@ char* clear_str(char *output_start) {
         *tilde_pos = '\0';
     }    
 
+    char *invite = strstr(output_start, "/ #");
+    if (invite != NULL) {
+        *invite = '\0';
+    } 
     
     while (*src) {
         if (in_escape) {
@@ -67,110 +187,4 @@ void drawGame() {
         DrawText(output_buf, 10, 40, 24, GREEN);
         DrawText(command, 10, 10, 24, GREEN);
     EndDrawing();
-}
-
-int main() {
-    InitWindow(800, 600, "IPC");
-
-    int stdin_pipe[2], stdout_pipe[2];    
-    if (pipe(stdin_pipe) == -1 || pipe(stdout_pipe) == -1) {
-        perror("pipe");
-        exit(1);
-    }
-
-    fcntl(stdout_pipe[0], F_SETFL, O_NONBLOCK);
-    fcntl(STDIN_FILENO, F_SETFL, O_NONBLOCK);
-
-    pid_t pid = fork();
-    if (pid == 0) {
-        close(stdin_pipe[1]);
-        close(stdout_pipe[0]);
-
-        dup2(stdin_pipe[0], STDIN_FILENO);
-        dup2(stdout_pipe[1], STDOUT_FILENO);
-        dup2(stdout_pipe[1], STDERR_FILENO);
-
-        close(stdin_pipe[0]);
-        close(stdout_pipe[1]);
-
-        execl("./tinyemu-2019-12-21/temu", "./temu", "-ctrlc", "./conf/root-riscv64.cfg", NULL);
-        perror("execl");
-        exit(1);
-    } else {
-        close(stdin_pipe[0]);
-        close(stdout_pipe[1]);
-
-        printf("Temu запущен (PID: %d)\n", pid);
-        printf("Готов к приему команд:\n");
-
-        while (!WindowShouldClose()) {
-            /*----------обработка ввода от пользователя----------*/
-            char ch;
-            ssize_t bytes = read(STDIN_FILENO, &ch, 1);
-            if (bytes > 0) {
-                if (ch == '\n' || ch == '\r') {
-                    command[cmd_pos] = '\0';
-                    
-                    if (strcmp(command, "exit") == 0) {
-                        break;
-                    }
-                    
-                    strcpy(last_command, command);
-                    
-                    output_buf[0] = '\0';
-
-                    write(stdin_pipe[1], command, cmd_pos);
-                    write(stdin_pipe[1], "\n", 1);
-                    cmd_pos = 0;
-
-                } else if (cmd_pos < sizeof(command) - 1) {
-                    command[cmd_pos++] = ch;
-                }
-            } else if (bytes < 0 && errno != EAGAIN) {
-                perror("read stdin");
-            }
-            /*----------конец обработки ввода----------*/
-
-            /*----------начало чтения----------*/
-            char buffer[1024];
-            bytes = read(stdout_pipe[0], buffer, sizeof(buffer)-1);
-            if (bytes > 0) {
-                buffer[bytes] = '\0';
-                char *cleaned = clear_str(buffer);
-
-                if (strlen(output_buf) + strlen(cleaned) < sizeof(output_buf) - 1) {
-                    strcat(output_buf, cleaned);
-                } else {
-                    memcpy(output_buf, buffer, bytes);
-                }
-
-                printf("buffer: '%s'\n", buffer);
-                printf("cleaned: '%s'\n", cleaned);
-
-                // printf("%s", cleaned);
-                fflush(stdout);
-            } else if (bytes < 0 && errno != EAGAIN) {
-                perror("read stdout");
-                break;
-            } else if (bytes == 0) {
-                printf("Temu завершил работу\n");
-                break;
-            }
-            /*----------завершение чтения----------*/
-
-            drawGame();
-        }
-
-        close(stdin_pipe[1]);
-        close(stdout_pipe[0]);
-
-        int status;
-        if (waitpid(pid, &status, 0)) {
-            printf("Child exit status - %d\n", WEXITSTATUS(status));
-        }
-        printf("Temu завершен.\n");
-    }
-
-    CloseWindow();
-    return 0;
 }
