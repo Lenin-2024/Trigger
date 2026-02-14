1984d88a06325c7cc02513acaa5fee8cee50ea74
Lenin-2024 <amelkin.misha@mail.ru>
2026-02-14 18:08:41 +0300

    add sh script and remake system

---
+++ start_temu.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo ip link delete tap0 2>/dev/null
+sudo ip tuntap add mode tap user $USER name tap0
+sudo ip addr add 192.168.3.1/24 dev tap0
+
+sudo ip link set tap0 up
+
+echo "===Проверка tap0==="
+ip addr show tap0
+
+
+./tinyemu-2019-12-21/temu -ctrlc ./conf/root-riscv64.cfg
